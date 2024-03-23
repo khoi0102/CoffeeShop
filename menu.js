fe39cced1c7316989a0edf93d2002595c78ec3f0
Khoi <lethanhkhoi01@gmail.com>
2024-03-23 15:42:43 -0500

    Add a price to the shopping cart and the quantity of items, add a remove quantity items and total price when click remove

--- menu.js
+++ menu.js
@@ -8,18 +8,35 @@ document.addEventListener("DOMContentLoaded", function (){
     function addToCart(price){
         cartCount++;
         totalPrice += price;
-
-        cartCountElement.textContent = cartCount;
-        cartTotalPriceElement.textContent = `$${totalPrice.toFixed(2)}`;
+        updateCartDisplay();
     }
-
+function updateCartDisplay(){
+        cartCountElement.textContent = cartCount;
+        cartTotalPriceElement.textContent = `$${totalPrice.toFixed(2)}`
+}
+function removeFromCart(price){
+        if (cartCount>0){
+            cartCount--;
+            totalPrice -= price;
+            if (totalPrice < 0) totalPrice = 0;
+            updateCartDisplay()
+        }
+}
 
     menuItem.forEach(item=>{
-        item.addEventListener('click',function (){
+        item.addEventListener('click',function (e){
+            if (e.target.classList.contains('remove-item'))
+            {
+                return;
+            }
             const price = parseFloat(item.getAttribute('data-price'));
             addToCart(price);
         })
+        const removeButton = item.querySelector('.remove-item');
+        removeButton.addEventListener('click',function(){
+            const price = parseFloat(item.getAttribute('data-price'));
+            removeFromCart(price);
+        })
     })
-
 })
 
